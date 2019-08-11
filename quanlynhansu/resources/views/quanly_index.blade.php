e97ac41ea523ff6192d1ba89eb99ffe3922e6b3b
NamBX <bxnam1996@gmail.com>
2019-08-12 01:53:42 +0700

    Xay dung modal them du lieu va ham ajax xu ly

--- quanlynhansu/resources/views/quanly_index.blade.php
+++ quanlynhansu/resources/views/quanly_index.blade.php
@@ -38,7 +38,54 @@
 
  </body>
 </html>
-
+<div id="formModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Thêm mới</h4>
+      </div>
+      <div class="modal-body">
+        <span id="form_result"></span>
+        <form method="post" id="sample_form" class="form-horizontal" enctype="multipart/form-data">
+          @csrf
+          <div class="form-group">
+            <label class="control-label col-md-4" >Họ Tên : </label>
+            <div class="col-md-8">
+              <input type="text" name="hoTen" id="hoTen" class="form-control" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-4">Địa Chỉ : </label>
+            <div class="col-md-8">
+              <input type="text" name="diaChi" id="diaChi" class="form-control" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-4">Tuổi : </label>
+            <div class="col-md-8">
+              <input type="text" name="tuoi" id="tuoi" />
+              <span id="store_image"></span>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-4">Số điện Thoại : </label>
+            <div class="col-md-8">
+              <input type="text" name="sdt" id="sdt" />
+              <span id="store_image"></span>
+            </div>
+          </div>
+          <br />
+          <div class="form-group" align="center">
+            <input type="hidden" name="action" id="action" />
+            <input type="hidden" name="hidden_id" id="hidden_id" />
+            <input type="submit" name="action_button" id="action_button" class="btn btn-warning" value="Add" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 <script>
 $(document).ready(function(){
 
@@ -60,7 +107,7 @@ $(document).ready(function(){
    },
    {
     data: 'tuoi',
-    name: 'tuoiZ'
+    name: 'tuoi'
    },
    {
     data: 'sdt',
@@ -74,13 +121,47 @@ $(document).ready(function(){
   ]
  });
 
-  
+  $('#create_record').click(function(){
+  $('.modal-title').text("Thêm mới");
+     $('#action_button').val("Thêm");
+     $('#action').val("Add");
+     $('#formModal').modal('show');
+ });
 
-
-
- 
-
-
-
+ $('#sample_form').on('submit', function(event){
+  event.preventDefault();
+  if($('#action').val() == 'Add')
+  {
+   $.ajax({
+    url:"{{ route('quanlynhansu.store') }}",
+    method:"POST",
+    data: new FormData(this),
+    contentType: false,
+    cache:false,
+    processData: false,
+    dataType:"json",
+    success:function(data)
+    {
+     var html = '';
+     if(data.errors)
+     {
+      html = '<div class="alert alert-danger">';
+      for(var count = 0; count < data.errors.length; count++)
+      {
+       html += '<p>' + data.errors[count] + '</p>';
+      }
+      html += '</div>';
+     }
+     if(data.success)
+     {
+      html = '<div class="alert alert-success">' + data.success + '</div>';
+      $('#sample_form')[0].reset();
+      $('#user_table').DataTable().ajax.reload();
+     }
+     $('#form_result').html(html);
+    }
+   })
+  }
+ });
 });
 </script>
